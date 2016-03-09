b03faee2bdf32a80ffbc23bf62e3c7a8a53fc5f8
Pawel Zawadzki <pawel.zawadzki@britishcouncil.pl>
2016-03-09 14:05:53 +0100

    Changes in file

--- src/plugins/file.ts
+++ src/plugins/file.ts
@@ -1,13 +1,22 @@
 import {Plugin, Cordova} from './plugin';
 import {Observable} from 'rxjs/Observable';
 
+export enum LocalFileSystem{
+    TEMPORARY = 0,
+    PERSISTENT = 1
+}
+
+export class FileSystem {
+    public name: string;
+}
+
 @Plugin({
     plugin: 'cordova-plugin-file',
-    pluginRef: 'cordova.file'
-})
+    pluginRef: ''
+})    
 export class File {
     @Cordova()
-    static getFreeDiskSpace() {
-        return new Promise<string>((res, rej) => { });
+    static requestFileSystem(type : LocalFileSystem, size:number) {
+        return new Promise<FileSystem>((res, rej) => { });
     }
 }
