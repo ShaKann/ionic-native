180427c8acdaa77d7e2d47df611364a6f678c4a0
Pawel Zawadzki <pawel.zawadzki@britishcouncil.pl>
2016-03-09 15:36:40 +0100

    Util - get change

--- dist/util.js
+++ dist/util.js
@@ -1,5 +1,8 @@
 "use strict";
 function get(obj, path) {
+    if (!path) {
+        return obj;
+    }
     for (var i = 0, path = path.split('.'), len = path.length; i < len; i++) {
         if (!obj) {
             return null;
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,11 @@
 export function get(obj, path) {
-  for(var i = 0, path = path.split('.'), len = path.length; i < len; i++) {
-    if(!obj) { return null; }
-    obj = obj[path[i]];
-  }
-  return obj;
+    if (!path) {
+        return obj;
+    }
+
+    for (var i = 0, path = path.split('.'), len = path.length; i < len; i++) {
+        if (!obj) { return null; }
+        obj = obj[path[i]];
+    }
+    return obj;
 };
